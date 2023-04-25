e77d748655142582a0bf1ccdf07f2c2fb4af2b3d
isuQuo <isuquo@gmail.com>
2023-04-26 00:16:48 +1000

    separate logic and fix top trends output to include payments and expenses

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,10 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math"
 	"os"
-	"sort"
-	"time"
 )
 
 type application struct {
@@ -46,104 +43,50 @@ func main() {
 		transactions: &transactions,
 	}
 
+	// -ex flag
 	if *excludeTransactionsPtr != "" {
 		transactions := app.excludeTransactions(*excludeTransactionsPtr)
 		app.transactions = &transactions
 	}
 
+	// -in flag
 	if *includeTransactionsPtr != "" {
 		transactions := app.includeTransactions(*includeTransactionsPtr)
 		app.transactions = &transactions
 	}
 
+	// -ga flag
 	if *greaterAmountPtr != 0 {
 		transactions := app.greaterAmount(*greaterAmountPtr)
 		app.transactions = &transactions
 	}
 
+	// -la flag
 	if *lesserAmountPtr != 0 {
 		transactions := app.lesserAmount(*lesserAmountPtr)
 		app.transactions = &transactions
 	}
 
-	// Test for flags and call correct function
+	// After filtering transactions, check if there are any left
+	if len(*app.transactions) == 0 {
+		fmt.Println("No transactions found")
+		return
+	}
+
+	// -e flag
 	if *totalExpensesPtr {
 		totalExpenses, totalIncome := app.calculateTotalExpensesAndIncome()
 		fmt.Printf("Total Expenses: $%.2f\n", totalExpenses)
 		fmt.Printf("Total Income: $%.2f\n", totalIncome)
 	}
 
+	// -t flag
 	if *topTrendsPtr != 0 {
-		topTrends := app.findTopTrends(*topTrendsPtr, false)
-		fmt.Println("Top Trends:")
-		for _, trend := range topTrends {
-			fmt.Printf("  %s: $%.2f\n", trend.Description, trend.TotalAmount)
-		}
+		app.printTopTrends(*topTrendsPtr, false)
 	}
 
+	// -x flag
 	if *topTrendsXPtr != 0 {
-		// Get earliest and latest dates
-		earliestDate := transactions[0].Date
-		latestDate := transactions[len(transactions)-1].Date
-
-		// Calculate the number of weeks between the two dates and split into 4 weeks
-		weeks := math.Abs(latestDate.Sub(earliestDate).Hours() / 24 / 7)
-		// Create a slice of the start dates of each week every 4 weeks
-		startEndDates := make(map[time.Time]time.Time)
-		// This loop will create a map of start dates and end dates for each 4 week period
-		for i := 0; i < int(weeks); i += 4 {
-			startDate := earliestDate.AddDate(0, 0, -(i+4)*7)
-			endDate := earliestDate.AddDate(0, 0, -i*7)
-			startEndDates[startDate] = endDate
-		}
-
-		// Sort startEndDates by start date and loop through startDate and endDate chronologically
-		var keys []time.Time
-		for k := range startEndDates {
-			keys = append(keys, k)
-		}
-		sort.Slice(keys, func(i, j int) bool {
-			return keys[i].Before(keys[j])
-		})
-
-		// Filter transactions by each 4 week period and calculate the top trends
-		for key := range keys {
-			startDate := keys[key]
-			endDate := startEndDates[startDate]
-
-			oldTransactions := app.transactions
-			filteredTransactions := app.filterByDateRange(startDate, endDate)
-
-			app.transactions = &filteredTransactions
-
-			// Loop through both payments and expenses
-			for _, txType := range []bool{true, false} {
-				// Filter transactions based on whether they are payments or expenses
-				filteredTypeTransactions := Transactions{}
-				for _, transaction := range filteredTransactions {
-					if (transaction.Amount >= 0) == txType {
-						filteredTypeTransactions = append(filteredTypeTransactions, transaction)
-					}
-				}
-
-				app.transactions = &filteredTypeTransactions
-				topTrends := app.findTopTrends(*topTrendsXPtr, txType)
-
-				// Check if there are any trends before printing the title and trends
-				if len(topTrends) > 0 {
-					if txType {
-						fmt.Printf("Top Payments Trends for %s to %s:\n", startDate.Format("02-01-2006"), endDate.Format("02-01-2006"))
-					} else {
-						fmt.Printf("Top Expenses Trends for %s to %s:\n", startDate.Format("02-01-2006"), endDate.Format("02-01-2006"))
-					}
-					for _, trend := range topTrends {
-						fmt.Printf("  %s: $%.2f\n", trend.Description, trend.TotalAmount)
-					}
-					fmt.Print("\n")
-				}
-			}
-
-			app.transactions = oldTransactions
-		}
+		app.printTopTrends(*topTrendsXPtr, true)
 	}
 }
--- cmd/cli/trend.go
+++ cmd/cli/trend.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"sort"
+	"time"
 )
 
 type Trend struct {
@@ -53,4 +56,95 @@ func (app *application) findTopTrends(topX int, isPayment bool) Trends {
 	}
 
 	return trends
+}
+
+func (app *application) generateStartEndDates() (map[time.Time]time.Time, []time.Time) {
+	earliestDate := (*app.transactions)[0].Date
+	latestDate := (*app.transactions)[len(*app.transactions)-1].Date
+
+	weeks := math.Abs(latestDate.Sub(earliestDate).Hours() / 24 / 7)
+	startEndDates := make(map[time.Time]time.Time)
+	for i := 0; i < int(weeks); i += 4 {
+		startDate := earliestDate.AddDate(0, 0, -(i+4)*7)
+		endDate := earliestDate.AddDate(0, 0, -i*7)
+		startEndDates[startDate] = endDate
+	}
+
+	var keys []time.Time
+	for k := range startEndDates {
+		keys = append(keys, k)
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		return keys[i].Before(keys[j])
+	})
+
+	return startEndDates, keys
+}
+
+// trends.go
+func (app *application) printTopTransactionTrends(startDate, endDate time.Time, topX int, isPayment bool) {
+	oldTransactions := app.transactions
+	filteredTransactions := app.filterByDateRange(startDate, endDate)
+
+	// Filter transactions based on whether they are payments or expenses
+	filteredTypeTransactions := Transactions{}
+	for _, transaction := range filteredTransactions {
+		if (transaction.Amount >= 0) == isPayment {
+			filteredTypeTransactions = append(filteredTypeTransactions, transaction)
+		}
+	}
+
+	app.transactions = &filteredTypeTransactions
+	topTrends := app.findTopTrends(topX, isPayment)
+
+	if len(topTrends) > 0 {
+		if isPayment {
+			fmt.Printf("Top Payments Trends for %s to %s:\n", startDate.Format("02-01-2006"), endDate.Format("02-01-2006"))
+		} else {
+			fmt.Printf("Top Expenses Trends for %s to %s:\n", startDate.Format("02-01-2006"), endDate.Format("02-01-2006"))
+		}
+		var total float64
+		for _, trend := range topTrends {
+			fmt.Printf("  %s: $%.2f\n", trend.Description, trend.TotalAmount)
+			total += trend.TotalAmount
+		}
+		fmt.Printf("Total: $%.2f\n", total)
+		fmt.Print("\n")
+	}
+
+	app.transactions = oldTransactions
+}
+
+func (app *application) printTopTrends(topX int, filterByDate bool) {
+	var keys []time.Time
+	var startEndDates map[time.Time]time.Time
+	if filterByDate {
+		startEndDates, keys = app.generateStartEndDates()
+	} else {
+		keys = make([]time.Time, 1)
+	}
+
+	for _, key := range keys {
+		var startDate, endDate time.Time
+		if filterByDate {
+			startDate = key
+			endDate = startEndDates[startDate]
+		} else {
+			// If filterByDate is false, set startDate and endDate to the earliest and latest transaction dates
+			startDate = (*app.transactions)[len(*app.transactions)-1].Date
+			endDate = (*app.transactions)[0].Date
+		}
+
+		oldTransactions := app.transactions
+		if filterByDate {
+			filteredTransactions := app.filterByDateRange(startDate, endDate)
+			app.transactions = &filteredTransactions
+		}
+
+		for _, txType := range []bool{true, false} {
+			app.printTopTransactionTrends(startDate, endDate, topX, txType)
+		}
+
+		app.transactions = oldTransactions
+	}
 }
